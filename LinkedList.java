0a75a1a3826751c682134a0af3cb204c98814076
Digya Mandala Putra <digyamp@gmail.com>
2019-12-25 00:51:10 +0700

    -

---
+++ LinkedList.java
@@ -0,0 +1,110 @@
+/**
+ * LinkedList
+ */
+package linked_list;
+
+public class LinkedList<T> {
+
+   private Node<T> head;
+
+   private int length = 0;
+
+   public static class Node<T> {
+
+      private T value;
+
+      private Node next;
+
+      public Node(final T val) {
+         setValue(val);
+
+         setNext(null);
+      }
+
+      public Node getNext() {
+         return next;
+      }
+
+      public void setNext(final Node next) {
+         this.next = next;
+      }
+
+      public T getValue() {
+         return value;
+      }
+
+      public void setValue(final T value) {
+         this.value = value;
+      }
+   }
+
+   public LinkedList() {
+   }
+
+   public int getLength() {
+      return this.length;
+   }
+
+   public LinkedList<T> pushBack(final T value) {
+      final Node newNode = new Node(value);
+
+      if (this.head == null) {
+         this.head = newNode;
+      } else {
+         Node curr = this.head;
+         while (curr.getNext() != null) {
+            curr = curr.getNext();
+         }
+         curr.setNext(newNode);
+      }
+      length = length + 1;
+      return this;
+   }
+
+   public LinkedList<T> remove(final int index) {
+
+      Node del;
+      Node curr;
+      if (index >= this.length) {
+         return this;
+      }
+
+      if (index == 0) {
+         del = this.head;
+         this.head = this.head.getNext();
+      } else {
+         int i = 0;
+         curr = this.head;
+         while (i < index - 1) {
+            curr = curr.getNext();
+            i = i + 1;
+         }
+         del = curr.getNext();
+         curr.setNext(del.getNext());
+      }
+      del = null;
+      length = length - 1;
+      return this;
+   }
+
+   public <T> T at(LinkedList<T> linkedList, final int index) {
+
+      Node curr = linkedList.head;
+      int i = 0;
+      while (i < index && i < linkedList.length) {
+         curr = curr.getNext();
+         i = i + 1;
+      }
+
+      return (T) curr.getValue();
+   }
+
+   public void printAll() {
+      Node curr = this.head;
+
+      while (curr != null) {
+         System.out.println(curr.getValue());
+         curr = curr.getNext();
+      }
+   }
+}
---
+++ Main.java
@@ -0,0 +1,25 @@
+/**
+ * Main
+ */
+package linked_list;
+
+public class Main {
+
+   public static void main(final String[] args) {
+      LinkedList<Integer> x = new LinkedList<Integer>();
+
+      x = x.pushBack(5);
+      x = x.pushBack(8);
+      x = x.pushBack(10);
+      x = x.pushBack(7);
+      x = x.pushBack(2);
+      x.printAll();
+
+      System.out.println();
+      System.out.println(x.at(x, 4));
+
+      x = x.remove(4);
+      System.out.println();
+      x.printAll();
+   }
+}
